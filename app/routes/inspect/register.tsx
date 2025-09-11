b2f9d02fa59414e1b54f35af9cb3bdeb85e5166f
Jared Gibson <jared.gibson@live.com>
2025-09-11 16:10:26 -0600

    Fix sizing and spacing on register page for mobile

--- app/routes/inspect/register.tsx
+++ app/routes/inspect/register.tsx
@@ -61,8 +61,8 @@ export default function InspectRegister({
   };
 
   return (
-    <div className="flex h-full w-full max-w-sm flex-col items-center justify-center self-center">
-      <div className="h-full max-h-[36rem] w-full">
+    <div className="my-8 flex h-full w-full max-w-sm flex-col items-center justify-center self-center">
+      <div className="h-[42rem] max-h-[calc(100vh-10rem)] w-full">
         <RegisterAssetAssistant
           assetId={tag?.asset?.id}
           canRegister={canRegister}
