528a00dbb737a38b2ecbdaa856994b01ba428bb4
Jared Gibson <jared.gibson@live.com>
2025-12-09 17:02:06 -0700

    Add missing sentry project config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const sentryConfig: SentryReactRouterBuildOptions = {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
   // An auth token is required for uploading source maps;
   // store it in an environment variable to keep it secure.
   authToken: process.env.SENTRY_AUTH_TOKEN,
